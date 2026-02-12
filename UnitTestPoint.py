3cedde3dc7b917c588fb6cf2d9f1d5865f9bba81
rkuouvup <rkuouvup@gmail.com>
2026-02-12 11:11:16 -0700

    Feb 12 in-class exercise

---
+++ UnitTestPoint.py
@@ -0,0 +1,34 @@
+from Point import Point
+import unittest
+
+class TestPoint(unittest.TestCase):
+    def test_Point_Default(self) -> None:
+        p = Point()
+        self.assertEqual(str(p), "(0, 0)")
+        self.assertAlmostEqual(p.x, 0)
+        self.assertEqual(p.y, 0)
+    def test_Point_Provided(self) -> None:
+        p = Point(3.0, 4.0)
+        self.assertEqual(str(p), "(3.0, 4.0)")
+    def test_Point_Update(self) -> None:
+        p = Point()
+        p.move(3.0, 4.0)
+        self.assertEqual(str(p), "(3.0, 4.0)")
+    def test_Point_Reset(self) -> None:
+        p = Point(3.0, 4.0)
+        p.reset()
+        self.assertEqual(p.x, 0)
+        self.assertEqual(p.y, 0)
+    def test_Point_CalDistance(self) -> None:
+        p1 = Point()
+        p2 = Point(3.0, 4.0)
+        d = p1.calculate_distance(p2)
+        self.assertEqual(d, 5)
+        p1.move(3.2, 5.9)
+        p2.move(2.4, 8.7)
+        d = p2.calculate_distance(p1)
+        self.assertAlmostEqual(d, 2.91, 2)
+
+
+if __name__ == "__main__":
+    unittest.main()
--- generator_ex.py
+++ generator_ex.py
@@ -24,6 +24,21 @@ def main() -> None:
     g = infinit_sequence()
     for i in range(5):
         print(next(g))
+    print("==== List Comprehension =====")
+    l = [num * 2 for num in range(5)]
+    print(l)
+    myList = [5, 2, 7, 6, 9, 1]
+    l = [e * 2 for e in myList if e % 2 == 0]
+    print(l)
+    print("==== Generator Expression =====")
+    g1 = (num * 2 for num in range(5))
+    print(g1)
+    for e in g1:
+        print(e)
+    g2 = (e * 2 for e in myList if e % 2 == 0)
+    print(g2)
+    for e in g2:
+        print(e)
 
 if __name__ == "__main__":
     main()
